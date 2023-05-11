6a1739d2d5abab56d3b4dc4474482eb58c3a3ff6
whitelintdev <108658597+whitelintdev@users.noreply.github.com>
2023-05-11 12:24:24 +0530

    Update fetchRoute.js

--- routes/fetchRoute.js
+++ routes/fetchRoute.js
@@ -9,4 +9,8 @@ router.post('/post',fetchController.createData)
 router.post('/user',fetchController.createUser)
 router.get('/fetchuser',fetchController.fetchUser)
 router.post('/login',fetchController.login)
+router.delete('/security/:id',fetchController.deleteSecurityProduct)
+router.delete('/investigation/:id',fetchController.deleteInvestigationProduct)
+router.delete('/user/:id',fetchController.deleteUser)
+
 module.exports = router;
